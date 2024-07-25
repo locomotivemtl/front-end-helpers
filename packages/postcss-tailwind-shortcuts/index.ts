023d08913404407d4a20ad87e505a723608a82ad
Deven Caron <devencaron@gmail.com>
2024-07-25 14:21:17 -0400

    Run prettier format

--- packages/postcss-tailwind-shortcuts/index.ts
+++ packages/postcss-tailwind-shortcuts/index.ts
@@ -1,4 +1,3 @@
-
 import { CustomThemeConfig } from 'tailwindcss/types/config';
 
 const tailwindAliasesMap = [
@@ -21,51 +20,67 @@ const tailwindAliasesMap = [
     {
         functionIdent: 'spacing',
         tailwindKey: 'spacing',
-    }
+    },
 ];
 
-
-const postcssTailwindShortcuts = (tailwindThemeConfig: Partial<CustomThemeConfig>) => {
+const postcssTailwindShortcuts = (
+    tailwindThemeConfig: Partial<CustomThemeConfig>
+) => {
     return {
         postcssPlugin: 'postcss-tailwind-shortcuts',
 
-        Root (root) {
-            root.walkDecls(decl => {
+        Root(root) {
+            root.walkDecls((decl) => {
                 // Create a concatenated regular expression pattern for all functionIdent values
-                const functionIdents = tailwindAliasesMap.map(alias => alias.functionIdent).join('|');
-                const regex = new RegExp(`\\b(${functionIdents})\\s*\\(\\s*['"]?([^'")]+)?['"]?\\s*\\)`, 'g');
+                const functionIdents = tailwindAliasesMap
+                    .map((alias) => alias.functionIdent)
+                    .join('|');
+                const regex = new RegExp(
+                    `\\b(${functionIdents})\\s*\\(\\s*['"]?([^'")]+)?['"]?\\s*\\)`,
+                    'g'
+                );
 
                 // Replace the function with the Tailwind value
-                decl.value = decl.value.replace(regex, (match, ident, value) => {
-                    // Find the corresponding tailwind key and get the value
-                    const alias = tailwindAliasesMap.find(alias => alias.functionIdent === ident);
+                decl.value = decl.value.replace(
+                    regex,
+                    (match, ident, value) => {
+                        // Find the corresponding tailwind key and get the value
+                        const alias = tailwindAliasesMap.find(
+                            (alias) => alias.functionIdent === ident
+                        );
 
-                    if (alias) {
-                        const tailwindKey = alias.tailwindKey;
-                        const defaultValue = 'default'; // Default key value to use if no value is provided
+                        if (alias) {
+                            const tailwindKey = alias.tailwindKey;
+                            const defaultValue = 'default'; // Default key value to use if no value is provided
 
-                        // Determine the value to use
-                        const keyValue = value || defaultValue;
+                            // Determine the value to use
+                            const keyValue = value || defaultValue;
 
-                        // Access the value from the Tailwind config
-                        const tailwindValue = tailwindThemeConfig.extend[tailwindKey]?.[keyValue];
+                            // Access the value from the Tailwind config
+                            const tailwindValue =
+                                tailwindThemeConfig.extend[tailwindKey]?.[
+                                    keyValue
+                                ];
 
-                        // Log an error if no value is found
-                        if (!tailwindValue) {
-                            console.error(`No value found for ${tailwindKey}.${keyValue}`);
-                            return match;
+                            // Log an error if no value is found
+                            if (!tailwindValue) {
+                                console.error(
+                                    `No value found for ${tailwindKey}.${keyValue}`
+                                );
+                                return match;
+                            }
+
+                            // Return the Tailwind value
+                            return tailwindValue;
                         }
 
-                        // Return the Tailwind value
-                        return tailwindValue;
+                        // Return the original value if no match is found
+                        return match;
                     }
-
-                    // Return the original value if no match is found
-                    return match;
-                });
+                );
             });
-        }
-    }
+        },
+    };
 };
 
 postcssTailwindShortcuts.postcss = true;
